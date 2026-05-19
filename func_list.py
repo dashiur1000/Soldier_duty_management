22672bd3da6b3a6ef3b288add2e24df05cc50758
david stern <dzs101010@gmail.com>
2026-05-19 21:58:45 +0300

    Writing a list of functions

---
+++ func_list.py
@@ -0,0 +1,114 @@
+# main.py
+def print_menu():
+    """
+    A function that prints the menu
+    on which the entire diet is based
+    """
+    pass
+
+
+def calling_and_running_functions(choice):
+    """
+    Takes the selection and uses it to activate
+    the desired function from the menu
+    """
+    pass
+
+
+def printing_operation_completed(gerund):
+    """
+    Print "Operation ____ completed successfully"
+    """
+    pass
+
+
+def exit_test():
+    """
+    Checking whether the user
+    wants to exit the program
+    """
+    pass
+
+
+# utils.py
+def receiving_input(choice):
+    """
+    A function that receives input from the user
+    and runs the health check function
+    """
+    pass
+
+
+def health_check(typing, choice):
+    """
+    Checking the integrity of the input
+    according to the required integrity definition
+    """
+    pass
+
+
+def search_by_id():
+    """
+    Search for a soldier by his ID, asks for a number
+    and saves it in the variable "id_number"
+    """
+    pass
+
+
+# soldier_manager.py
+def adding_a_soldier():
+    """
+    A function that adds a soldier to the list of soldiers.
+    The function receives an ID number, checks its validity,
+    requests a name, and saves the details
+    """
+    pass
+
+
+def remove_soldier():
+    """
+    A function that removes a soldier from the list of soldiers.
+    The function receives an ID number, checks its validity,
+    and deletes the details
+    """
+    pass
+
+
+def printing_a_list_of_soldiers():
+    """
+    A function that goes through all the soldiers
+    and prints the names and numbers
+    """
+    pass
+
+
+def search_by_name():
+    """
+    A function that searches
+    for a soldier by name and not by ID
+    """
+    pass
+
+
+# duty_manager.py
+def add_a_shift():
+    """
+    A function that activates a search function by name
+    and updates shifts and saves
+    """
+    pass
+
+
+def update_duty_status():
+    """
+    A function that activates the soldier's duty printing function
+    and updates the duty execution status
+    """
+    pass
+
+
+def printing_shifts_by_soldier():
+    """
+    A function that prints the shifts that appear for a specific soldier
+    """
+    pass
